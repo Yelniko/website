7899ca240f1acdd22cd049f4875384ba302ee1ae
Yelnikov Oleh <xoleg2006x@gmail.com>
2024-01-09 15:12:10 +0200

    p

--- website/web/urls.py
+++ website/web/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('catalog/', views.test, name='home'),
-    path('teg/', views.teg, name='teg')
+    path('', views.test, name='home'),
+    path('teg/', views.teg, name='task')
 ]
 
--- website/web/views.py
+++ website/web/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def test(request):
-    return render(request, 'web/pages/code/header.html')
+    return render(request, 'web/pages/code/fist.html')
 
 
 def teg(request):
-    return render(request, 'web/pages/code/teg.html')
+    return render(request, 'web/pages/code/tasks.html')
 
 
